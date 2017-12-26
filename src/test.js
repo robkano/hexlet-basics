ef493268fd75251860db3c65da8006e5d14ae4ff
Robin Koinov <robkano@gmail.com>
2017-12-26 12:29:15 +0200

    adding new files

---
+++ src/test.js
@@ -0,0 +1,19 @@
+const evenNumber = (num) => {
+  const firstNum = Math.floor(Math.random() * 40 / 2) * 2;
+  let queue = firstNum;
+  let i = 1;
+  let finelResult = '';
+  let hidedNum = '';
+  while (i < 8) {
+    if (i === 2) {
+      hidedNum = queue;
+      finelResult += '..';
+    }
+    finelResult += `${queue  } `;
+    queue += 2;
+    i++;
+  }
+  console.log(hidedNum);
+  return finelResult;
+};
+evenNumber();
